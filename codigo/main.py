3ef16708f8ef77a5325a28ca74f9766e44dd98c3
Felipe Morais <ddtankx68@gmail.com>
2020-04-19 09:41:10 -0300

    Primeiro exemplo.

---
+++ codigo/main.py
@@ -0,0 +1,30 @@
+from tkinter import Tk, Menu
+
+def about():
+    print("calling about")
+
+def main():
+    root = Tk()
+
+    #criando o menu
+    menubar=Menu(root)
+    root.config(menu=menubar)
+
+    #criando os sub-menu
+    subMenu=Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="File", menu=subMenu)
+    subMenu.add_command(label="New Project")
+    subMenu.add_command(label="Exit")
+
+    subMenuHelp=Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Help", menu=subMenuHelp)
+    subMenuHelp.add_command(label="About", command=about)
+
+
+
+    root.geometry("400x400+0+0")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
